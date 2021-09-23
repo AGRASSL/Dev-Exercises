b6245f7308594c3c04cd41787901c4589e47cada
Alexandra Grassl <eai.alexandra@gmail.com>
2021-09-22 22:55:54 -0500

    component quoteslist

---
+++ vue-quote-finder/src/components/QuotesList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Quotes list</h1>
+        <!-- <span>{{ quotesAmout }} Quotes</span> -->
+    <ul class="quotes">
+        <li v-for="quote in quotes" :key="quote.qt">
+            <h1>{{ quote.quote }}</h1>
+            <h2>{{ quote.context }}</h2>
+            <h3>{{ quote.theme }}</h3>
+        </li>
+    </ul>
+    </div>
+</template>
+
+<script>
+// import Vue from 'vue'
+// import axios from 'axios'
+// import VueAxios from  'vue-axios'
+
+// Vue.use(VueAxios,axios)
+import json from '../assets/quotes.json'
+export default {
+    name: "QuotesList",
+
+    data: function() {
+
+
+        return { 
+            quotes: json.quotes,
+             };
+ 
+    },
+
+}
+</script>
